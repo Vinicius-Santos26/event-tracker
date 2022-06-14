ab3aac76b14f1fe8e21a5acc8c593c3ee6a7f391
Vinicius Santos <vinicius.santos@inmetrics.com.br>
2022-06-14 08:44:43 -0300

    Fim 5 aula - seletores async e depuraçao

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,25 +1,11 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
 import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
+import { eventosAsync } from "./seletores";
 
 export const listaDeEventosState = atom<IEvento[]>({
     key: 'listadaDeEventosState',
-    default: [
-        {
-            "descricao": "Estudar React",
-            "inicio": new Date("2022-06-15T09:00"),
-            "fim": new Date("2022-06-15T13:00"),
-            "completo": false,
-            "id": 1642342747
-        },
-        {
-            "descricao": "Estudar Recoil",
-            "inicio": new Date("2022-06-16T09:00"),
-            "fim": new Date("2022-06-16T11:00"),
-            "completo": false,
-            "id": 1642342959
-        }
-    ]
+    default: eventosAsync
 });
 
 export const filtroDeEventosState = atom<IFiltroDeEventos>({
